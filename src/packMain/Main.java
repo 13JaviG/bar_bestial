13076afc93978fc5bcb262370accf2e9e71d5702
Ander Raso Vázquez <anderraso@gmail.com>
2018-03-11 18:32:37 +0100

    puesta en marcha del juego hasta la HU1
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

---
+++ src/packMain/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package packMain;
+
+import packControlador.Juego;
+
+/**
+ * Clase donde iniciar el juego.
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Juego.getJuego().juegoNuevo();
+	}
+
+}
