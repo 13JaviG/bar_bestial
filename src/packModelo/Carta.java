dc7f44ac19b2848bc215d4d0552f233e9345fe2a
Ander Raso Vázquez <anderraso@gmail.com>
2018-03-11 18:29:55 +0100

    implementada la clase carta
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

---
+++ src/packModelo/Carta.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package packModelo;
+
+/**
+ * Representa una carta en el juego.
+ *
+ */
+public class Carta {
+	
+	private int num;
+	private EnumColor color;
+	private Animal animal;
+	
+	public Carta(int pNum, EnumColor pColor, Animal pAnimal) {
+		num = pNum;
+		color = pColor;
+		animal = pAnimal;
+	}
+	
+	public void hacerAnimalada() {
+		animal.hacerAnimalada();
+	}
+	
+	/**
+	 * Devuelve un string con la información de la carta.
+	 */
+	public String toString() {
+		StringBuilder result = new StringBuilder();
+		result.append("Color: ");
+		result.append(color);
+		result.append(" | Number: ");
+		result.append(num);
+		result.append(" | Animal: ");
+		result.append(animal.getClass().getName());
+		return result.toString();
+	}
+	
+}
