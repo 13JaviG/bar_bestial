7c13b56c2b79cafcc5368181b96d932d5ed96d13
Ander Raso Vázquez <anderraso@gmail.com>
2018-05-11 11:24:34 +0200

    usa un dialogo para pedir input
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

--- src/packModelo/animales/Canguro.java
+++ src/packModelo/animales/Canguro.java
@@ -6,6 +6,8 @@ package packModelo.animales;
 import java.util.Random;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 import packControlador.Juego;
 import packModelo.Animal;
 import packModelo.Carta;
@@ -18,13 +20,16 @@ import packModelo.ColaDelBar;
 public class Canguro extends Animal {
 
 	public void hacerAnimalada(Carta pCarta) {
-		// TODO con scanner hasta que pongamos observers
 		if (pCarta.getColor() == Juego.jugadorColor) {
 			Scanner in = new Scanner(System.in);
 			int numSaltos = 0;
 			while ((numSaltos != 1) && (numSaltos != 2)) {
-				System.out.println("Cuantas cartas salta el canguro? (1 o 2)");
-				numSaltos = in.nextInt();
+				String textoDialogo = JOptionPane.showInputDialog("Cuantas cartas salta el canguro? (1 o 2)");
+				try {
+					numSaltos = Integer.parseInt(textoDialogo);
+				} catch (NumberFormatException e) {
+					numSaltos = -1;
+				}
 			}
 
 			for (int i = 0; i < numSaltos; i++) {
